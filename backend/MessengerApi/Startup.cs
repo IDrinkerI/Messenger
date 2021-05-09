ebd9658a8ee657d99b6c40698a57d234746784ed
Drinker <drink0.5l@gmail.com>
2021-05-09 10:05:27 +0300

    configured Startup for using controllers

--- backend/MessengerApi/Startup.cs
+++ backend/MessengerApi/Startup.cs
@@ -16,6 +16,7 @@ namespace MessengerApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers(config => config.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +26,7 @@ namespace MessengerApi
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseMvc();
 
             app.UseFileServer();
         }
