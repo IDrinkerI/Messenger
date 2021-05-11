2b03508f415049f26083dc196ac5365546f93a14
Drinker <drink0.5l@gmail.com>
2021-05-11 12:42:44 +0300

    now new message get value from InputField

--- frontend/src/components/Messenger.jsx
+++ frontend/src/components/Messenger.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import MessageModel from "../model/MessageModel";
+import { updateInputMessageAction } from "../store/input_message/actions";
+import { getInputMessage } from "../store/input_message/selectors";
+import { addMessageAction } from "../store/message/actions";
 import "../style/messenger.scss";
 import Button from "./Button.jsx";
 import ChatList from "./ChatList.jsx";
@@ -6,18 +11,27 @@ import InputField from "./InputField.jsx";
 import MessageField from "./MessageField.jsx";
 
 
-const Messenger = () => (
-    <div className="messenger">
-        <div className="messenger_inner">
-            <ChatList />
-            <MessageField />
-        </div>
+const Messenger = () => {
+    const dispatch = useDispatch();
+    const inputMessage = useSelector(getInputMessage);
+    const buttonHandler = () => {
+        dispatch(addMessageAction(new MessageModel("bot", inputMessage)));
+        dispatch(updateInputMessageAction(""));
+    }
 
-        <div className="messenger_inner">
-            <InputField />
-            <Button>Send</Button>
+    return (
+        <div className="messenger">
+            <div className="messenger_inner">
+                <ChatList />
+                <MessageField />
+            </div>
+
+            <div className="messenger_inner">
+                <InputField />
+                <Button onClick={buttonHandler}>Send</Button>
+            </div>
         </div>
-    </div>
-);
+    );
+}
 
 export default Messenger;
--- frontend/src/store/input_message/actions.js
+++ frontend/src/store/input_message/actions.js
@@ -1,4 +1,4 @@
 import { INPUT_MESSAGE_UPDATE } from "./types";
 
 
-export const updateInputMessageAction = (text) => ({type: INPUT_MESSAGE_UPDATE, payload: text});
+export const updateInputMessageAction = (text = "") => ({type: INPUT_MESSAGE_UPDATE, payload: text});
