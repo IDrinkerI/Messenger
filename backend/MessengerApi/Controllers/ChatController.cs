6990d78c4fb632194e8eda764d4405188ce82a1a
Drinker <drink0.5l@gmail.com>
2021-05-12 17:19:05 +0300

    added Controllers.ChatController

---
+++ backend/MessengerApi/Controllers/ChatController.cs
@@ -0,0 +1,40 @@
+﻿using MessengerApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessengerApi.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class ChatController : ControllerBase
+    {
+        private Store _store;
+
+        public ChatController(Store store)
+        {
+            _store = store;
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetChats()
+        {
+            var chats = await _store.Chats.ToListAsync();
+            return new JsonResult(chats);
+        }
+
+        [HttpPost]
+        public async Task<StatusCodeResult> AddChat([FromBody] Chat chat)
+        {
+            if (chat is null)
+                return new UnsupportedMediaTypeResult();
+
+            await _store.Chats.AddAsync(chat);
+            await _store.SaveChangesAsync();
+            return new OkResult();
+        }
+    }
+}
