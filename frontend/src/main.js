9bed6efd6c81709c7c0eca8435d2e080faa1f516
Drinker <drink0.5l@gmail.com>
2021-05-28 10:46:49 +0300

    rename main.jsx to main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import "regenerator-runtime/runtime";
 import React from "react";
 import ReactDom from "react-dom";
-import Application from "./components/Application.jsx";
+import Application from "./components/Application";
 import "./style/normalize.css";
 
 
