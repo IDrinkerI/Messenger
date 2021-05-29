3707b014c3b19d4b767148c69dbe1c840d833bab
Drinker <drink0.5l@gmail.com>
2021-05-29 09:31:20 +0300

    added initial Controllers.ProfileController

---
+++ backend/MessengerApi/Controllers/ProfileController.cs
@@ -0,0 +1,26 @@
+﻿using MessengerApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessengerApi.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class ProfileController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<JsonResult> GetProfile()
+        {
+            var profile = new Profile()
+            {
+                Id = 0,
+                Nickname = "Backend",
+            };
+
+            return new JsonResult(profile);
+        }
+    }
+}
