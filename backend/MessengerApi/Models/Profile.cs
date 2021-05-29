dbc645afe7bc262199187811a616fc866d9b689c
Drinker <drink0.5l@gmail.com>
2021-05-29 09:25:52 +0300

    added model Models.Profile

---
+++ backend/MessengerApi/Models/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessengerApi.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Nickname { get; set; }
+
+    }
+}
