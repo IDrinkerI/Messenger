d19951b0c5b5630a521545576c7122712b11d7a9
Drinker <drink0.5l@gmail.com>
2021-05-29 05:19:52 +0300

    connected profile store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,6 +3,7 @@ import thunk from "redux-thunk";
 import { chatsReducer } from "../components/ChatList";
 import { inputMessageReducer } from "../components/InputMessage";
 import { messageReduser } from "../components/MessageField";
+import { profileReducer } from "../components/Profile";
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -12,6 +13,7 @@ export const store = createStore(
         messages: messageReduser,
         inputText: inputMessageReducer,
         chats: chatsReducer,
+        profile: profileReducer,
     }),
     composeEnhancers(applyMiddleware(thunk))
 )
