0c7b281e9e8d5fb221450a3992581c460e125ff2
Drinker <drink0.5l@gmail.com>
2021-05-30 14:25:50 +0300

    Signin component complete

--- frontend/src/components/Signin/Singin.jsx
+++ frontend/src/components/Signin/Singin.jsx
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "../Button";
+import { HtmlContainer } from "../HtmlContainer";
+import { InputField } from "../InputField";
+import "./signin.scss";
 
+const API = "api/signin";
 
 export const Singin = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const emailOnChangeHandler = ({ target }) => {
+        const value = target.value;
+        setEmail(value);
+    }
+
+    const passwordOnChangeHandler = ({ target }) => {
+        const value = target.value;
+        setPassword(value);
+    }
+
+    const signinButtonHandler = async (event) => {
+        event.preventDefault();
+
+        const response = await fetch(API, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password }),
+        });
+
+        const signinResult = await response.json();
+
+        console.log("Signin result:", { signinResult });
+
+        setEmail("");
+        setPassword("");
+    }
 
     return (
-        <>
-            <h3>SingIn</h3>
-        </>
+        <HtmlContainer>
+            <form className="signin-inner">
+                <h3>SingIn</h3>
+                <InputField.Label label="Email:">
+                    <InputField value={email} onChange={emailOnChangeHandler} />
+                </InputField.Label>
+
+                <InputField.Label label="Password:">
+                    <InputField value={password} onChange={passwordOnChangeHandler} />
+                </InputField.Label>
+
+                <Button onClick={signinButtonHandler}>
+                    Signin
+                </Button>
+            </form>
+        </HtmlContainer>
     )
 }
