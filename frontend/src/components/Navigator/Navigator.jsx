4e742c4e70d873d3d40b71fc5ee52800c543ddc1
Drinker <drink0.5l@gmail.com>
2021-05-30 12:56:23 +0300

    Navigator is now in the HtmlContainer

--- frontend/src/components/Navigator/Navigator.jsx
+++ frontend/src/components/Navigator/Navigator.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router";
+import { HtmlContainer } from "../HtmlContainer";
 import "./navigator.scss";
 
 
@@ -9,11 +10,13 @@ export const Navigator = () => {
     const profileLinkClickHandler = () => history.push("/profile");
 
     return (
-        <nav>
-            <ul className="navigator-link_wrapper">
-                <li onClick={homeLinkClickHandler}>Home</li>
-                <li onClick={profileLinkClickHandler}>Profile</li>
-            </ul>
-        </nav>
+        <HtmlContainer>
+            <nav>
+                <ul className="navigator-link_wrapper">
+                    <li onClick={homeLinkClickHandler}>Home</li>
+                    <li onClick={profileLinkClickHandler}>Profile</li>
+                </ul>
+            </nav>
+        </HtmlContainer>
     );
 }
