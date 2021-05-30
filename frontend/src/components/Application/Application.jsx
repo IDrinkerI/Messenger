fffe7633fe89046954a9c09797795be903bc1a1a
Drinker <drink0.5l@gmail.com>
2021-05-30 13:13:00 +0300

    added Singin component in Router

--- frontend/src/components/Application/Application.jsx
+++ frontend/src/components/Application/Application.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { store } from "../../store";
 import { Messenger } from "../Messenger";
 import { Navigator } from "../Navigator";
 import { Profile } from "../Profile";
+import { Singin } from "../Signin";
 
 
 export const Application = () => (
@@ -16,9 +17,15 @@ export const Application = () => (
                     <Messenger />
                 </Route>
 
-                <Route path="/profile">
+                <Route exact path="/profile">
                     <Profile />
                 </Route>
+
+                <Route exact path="/singin">
+                    <Singin />
+                </Route>
+
+                <Redirect to="/" />
             </Switch>
         </BrowserRouter>
     </Provider>
