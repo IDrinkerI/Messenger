555d680ebb4b8e753898128d16c49a9a1611e284
Drinker <drink0.5l@gmail.com>
2021-06-01 06:01:44 +0300

    added create profile for new user

--- backend/MessengerApi/Controllers/SignupController.cs
+++ backend/MessengerApi/Controllers/SignupController.cs
@@ -23,10 +23,23 @@ namespace MessengerApi.Controllers
         [HttpPost]
         public async Task<IActionResult> Singup([FromBody] User newUser)
         {
-            if (newUser is null) { return BadRequest(new { errorText = "wrong user info" }); }
+            if (newUser is null ||
+                newUser.Email is null ||
+                newUser.Password is null)
+            {
+                return BadRequest(new { errorText = "wrong user info" });
+            }
 
             var user = await _store.Users.FirstOrDefaultAsync(u => u.Email == newUser.Email);
             if (!(user is null)) { return BadRequest(new { erroeText = "Than email is taken." }); }
+
+            var newProfile = new Profile()
+            {
+                Nickname = newUser.Email,
+            };
+
+            newUser.Profile = newProfile;
+            await _store.Profiles.AddAsync(newProfile);
 
             await _store.Users.AddAsync(newUser);
             await _store.SaveChangesAsync();
