09d7e2bd0bfd22b4fa3950c59cdd19fd8317dfbf
Drinker <drink0.5l@gmail.com>
2021-06-01 05:56:01 +0300

    added todo task

--- backend/MessengerApi/Models/Profile.cs
+++ backend/MessengerApi/Models/Profile.cs
@@ -10,6 +10,7 @@ namespace MessengerApi.Models
         public int Id { get; set; }
         public string Nickname { get; set; }
 
+        // TODO: rename method
         public void CopyFrom(Profile other)
         {
             Nickname = other.Nickname;
