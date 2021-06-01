2ee89a47b43ba240ea8dc1ff24915d68710e9c48
Drinker <drink0.5l@gmail.com>
2021-06-01 06:12:47 +0300

    added initial Signup component

---
+++ frontend/src/components/Signup/Signup.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { HtmlContainer } from "../HtmlContainer";
+import "./signup.scss";
+
+
+export const Signup = () => {
+
+    return (
+        <HtmlContainer>
+            <form className="singup-inner">
+                <h3>Singup</h3>
+
+            </form>
+        </HtmlContainer>
+    );
+}
