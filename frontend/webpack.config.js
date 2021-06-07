a4a74fe0658a35c724287041e2104dabfc4cd80c
Drinker <drink0.5l@gmail.com>
2021-06-07 16:09:52 +0300

    renamed project to Messenger.Api

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     entry: path.join(__dirname, "src", "main.js"),
     output: {
-        path: path.join(__dirname, "..", "backend/MessengerApi/wwwroot"),
+        path: path.join(__dirname, "..", "backend/Messenger.Api/wwwroot"),
         filename: "./main.js",
         publicPath: "/",
     },
