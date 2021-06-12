25471a01d10020b6d728067e2e1cc420b751494d
Drinker <drink0.5l@gmail.com>
2021-06-12 20:47:28 +0300

    UserRepository redisigned

---
+++ backend/Messenger.DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using Messenger.Data;
+using Messenger.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Messenger.DataAccess
+{
+    public sealed class UserRepository : IRepository<UserEntity>
+    {
+        private readonly StoreContext store;
+
+        public UserRepository(StoreContext store)
+        {
+            this.store = store;
+        }
+
+        async Task<UserEntity> IRepository<UserEntity>.Get(int id)
+        {
+            var user = await store.Users
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            return user;
+        }
+
+        async void IRepository<UserEntity>.Add(UserEntity item)
+        {
+            var newProfile = new ProfileEntity { Nickname = item.Email, };
+            item.Profile = newProfile;
+
+            await store.Users.AddAsync(item);
+            await store.SaveChangesAsync();
+        }
+
+        async void IRepository<UserEntity>.Update(int id, UserEntity newState)
+        {
+            var user = await store.Users
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if (user is null) { return; }
+
+            user.UpdateState(newState);
+            await store.SaveChangesAsync();
+        }
+
+        async Task<IEnumerable<UserEntity>> IRepository<UserEntity>.GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
