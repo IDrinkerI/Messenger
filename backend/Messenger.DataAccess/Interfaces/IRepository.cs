01e7e81b56316f97efeb195a208886f27735ebfc
Drinker <drink0.5l@gmail.com>
2021-06-12 20:00:38 +0300

    changed constraints for <T> in IRepository

--- backend/Messenger.DataAccess/Interfaces/IRepository.cs
+++ backend/Messenger.DataAccess/Interfaces/IRepository.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using Messenger.Data;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 
 namespace Messenger.DataAccess
 {
-    public interface IRepository<T> where T : class
+    public interface IRepository<T> where T : IEntity
     {
         Task<IEnumerable<T>> GetAll();
         Task<T> Get(int id);
