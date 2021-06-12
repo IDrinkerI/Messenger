42cec052adcf7f9162854349fdab41385e9339c5
Drinker <drink0.5l@gmail.com>
2021-06-12 06:48:10 +0300

    added Models.AuthInfo

---
+++ backend/Messenger.Store/Models/AuthInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.Store.Models
+{
+    public class AuthInfo
+    {
+        public int Id { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
