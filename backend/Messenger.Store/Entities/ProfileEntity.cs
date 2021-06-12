d2ad0739b762b6af90018bf923ab8b409e6813aa
Drinker <drink0.5l@gmail.com>
2021-06-12 20:26:43 +0300

    added method ProfileEntity.UpdateState()

--- backend/Messenger.Store/Entities/ProfileEntity.cs
+++ backend/Messenger.Store/Entities/ProfileEntity.cs
@@ -5,5 +5,12 @@
         public string Nickname { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public void UpdateState(ProfileEntity newState)
+        {
+            Nickname = newState.Nickname;
+            FirstName = newState.FirstName;
+            LastName = newState.LastName;
+        }
     }
 }
