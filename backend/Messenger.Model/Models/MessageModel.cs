395822691fa9a687ec58f79234b937797bb293a8
Drinker <drink0.5l@gmail.com>
2021-06-13 05:03:09 +0300

    MessageModel now derived from BaseModel

--- backend/Messenger.Model/Models/MessageModel.cs
+++ backend/Messenger.Model/Models/MessageModel.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Messenger.Model
 {
-    public sealed class MessageModel
+    public sealed class MessageModel : BaseModel
     {
         public string Text { get; set; }
         public string Nickname { get; set; }
