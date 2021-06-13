3e00bcddd524ee20f7b2a790232f271340f96dbb
Drinker <drink0.5l@gmail.com>
2021-06-13 04:32:40 +0300

    implemented IUpdatebleEntity in AuthInfoEntity

--- backend/Messenger.Store/Entities/AuthInfoEntity.cs
+++ backend/Messenger.Store/Entities/AuthInfoEntity.cs
@@ -1,7 +1,12 @@
 ﻿namespace Messenger.Data.Entities
 {
-    public class AuthInfoEntity : BaseEntity
+    public class AuthInfoEntity : BaseEntity, IUpdatableEntity<AuthInfoEntity>
     {
         public string PasswordHash { get; set; }
+
+        public void UpdateState(AuthInfoEntity newState)
+        {
+            PasswordHash = newState.PasswordHash;
+        }
     }
 }
