de6fa744a2fc737ead1b21c91909585b7aa195e7
Drinker <drink0.5l@gmail.com>
2021-06-13 04:40:40 +0300

    implemented IUpdatableEntity interface in SessionEntity

--- backend/Messenger.Store/Entities/SessionEntity.cs
+++ backend/Messenger.Store/Entities/SessionEntity.cs
@@ -1,10 +1,15 @@
 ﻿namespace Messenger.Data.Entities
 {
-    public class SessionEntity : BaseEntity
+    public class SessionEntity : BaseEntity, IUpdatableEntity<SessionEntity>
     {
         public string Token { get; set; }
         public System.DateTime KillingTime { get; set; }
         public int UserId { get; set; }
         public UserEntity User { get; set; }
+
+        public void UpdateState(SessionEntity newState)
+        {
+            KillingTime = newState.KillingTime;
+        }
     }
 }
