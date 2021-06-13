a48c4ce5100bdb98e98fca2ce573327c7ee9e28b
Drinker <drink0.5l@gmail.com>
2021-06-13 05:07:03 +0300

    added interface IService

---
+++ backend/Messenger.Service/Interface/IService.cs
@@ -0,0 +1,15 @@
+﻿using Messenger.Model;
+using System.Collections.Generic;
+
+
+namespace Messenger.Service.Services
+{
+    public interface IService<T> where T: IModel
+    {
+        void Add(T item);
+        T Get();
+        IEnumerable<T> GetAll();
+        void Update(int id, T newState);
+        void Delete(T item);
+    }
+}
