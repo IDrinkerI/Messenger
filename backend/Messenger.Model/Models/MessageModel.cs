f2bff8d6c23226618694c0902f8b838882abf883
Drinker <drink0.5l@gmail.com>
2021-06-13 04:54:03 +0300

    added MessageModel
    fix

---
+++ backend/Messenger.Model/Models/MessageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Messenger.Model
+{
+    public sealed class MessageModel
+    {
+        public string Text { get; set; }
+        public string Nickname { get; set; }
+        public int ProfileId { get; set; }
+        public int ChatId { get; set; }
+    }
+}
