169bffb35897d095cbd9689dd66491b8223c45a5
Drinker <drink0.5l@gmail.com>
2021-06-14 18:26:12 +0300

    implement ProfileService

--- backend/Messenger.Service/Services/ProfileService.cs
+++ backend/Messenger.Service/Services/ProfileService.cs
@@ -1,23 +1,35 @@
 ﻿using Messenger.Data.Entities;
+using Messenger.DataAccess;
 using Messenger.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Messenger.Mapper;
+
 
 namespace Messenger.Service
 {
     public class ProfileService
     {
+        private readonly IRepository<ProfileEntity> profileRepository;
+
+        public ProfileService(IRepository<ProfileEntity> profileRepository)
+        {
+            this.profileRepository = profileRepository;
+        }
+
         public async Task<ProfileModel> GetProfile(int id)
         {
-            throw new NotImplementedException();
+            var entity = await profileRepository.Get(id);
+            return entity.ToModel();
         }
 
         public async Task UpdateProfile(int id, ProfileModel value)
         {
-            throw new NotImplementedException();
+            var entity = value.ToEntity();
+            await profileRepository.Update(id, entity);
         }
     }
 }
