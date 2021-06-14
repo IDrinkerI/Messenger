9e7a28c0ee210c84437cfc5defb99d5038898a8e
Drinker <drink0.5l@gmail.com>
2021-06-14 19:01:24 +0300

    implemented MessageService

--- backend/Messenger.Service/Services/MessageService.cs
+++ backend/Messenger.Service/Services/MessageService.cs
@@ -1,4 +1,7 @@
-﻿using Messenger.Model;
+﻿using Messenger.Data.Entities;
+using Messenger.DataAccess;
+using Messenger.Mapper;
+using Messenger.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +13,28 @@ namespace Messenger.Service
 {
     public class MessageService
     {
+        private readonly IMessageRepository<MessageEntity> messageRepository;
+
+        public MessageService(IMessageRepository<MessageEntity> messageRepository)
+        {
+            this.messageRepository = messageRepository;
+        }
+
         async public Task<IEnumerable<MessageModel>> GetMessages(int chatId)
         {
-            throw new NotImplementedException();
+            var entities = await messageRepository.GetAll(chatId);
+
+            var messages = new List<MessageModel>();
+            foreach (var entity in entities)
+                messages.Add(entity.ToModel());
+
+            return messages;
         }
 
         async public Task AddMessage(MessageModel message)
         {
-            throw new NotImplementedException();
+            var messageEntity = message.ToEntity();
+            await messageRepository.Add(messageEntity);
         }
     }
 
