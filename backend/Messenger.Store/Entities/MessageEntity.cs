511807061f5d97e894620dc36f09380fa4ab64e4
Drinker <drink0.5l@gmail.com>
2021-06-14 18:22:53 +0300

    MessageEntity now implemented interface IUpdatableEntity

--- backend/Messenger.Store/Entities/MessageEntity.cs
+++ backend/Messenger.Store/Entities/MessageEntity.cs
@@ -1,11 +1,19 @@
-﻿namespace Messenger.Data.Entities
+﻿using System.Threading.Tasks;
+
+namespace Messenger.Data.Entities
 {
-    public sealed class MessageEntity : BaseEntity
+    public sealed class MessageEntity : BaseEntity, IUpdatableEntity<MessageEntity>
     {
         public string Text { get; set; }
         public int ProfileId { get; set; }
         public ProfileEntity Profile { get; set; }
         public int ChatId { get; set; }
         public ChatEntity Chat { get; set; }
+
+        public Task UpdateState(MessageEntity newState)
+        {
+            Text = newState.Text ;
+            return Task.CompletedTask;
+        }
     }
 }
