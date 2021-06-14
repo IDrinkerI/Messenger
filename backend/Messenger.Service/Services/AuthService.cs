7e85fcf380a70c1cc18c833bb409e97894d5f5c7
Drinker <drink0.5l@gmail.com>
2021-06-14 19:51:03 +0300

    implemented AuthService

--- backend/Messenger.Service/Services/AuthService.cs
+++ backend/Messenger.Service/Services/AuthService.cs
@@ -1,27 +1,66 @@
-﻿using Messenger.Model;
+﻿using Messenger.Data.Entities;
+using Messenger.DataAccess;
+using Messenger.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Messenger.Service
 {
     public class AuthService
     {
+        private readonly IRepository<AuthInfoEntity> authInfoRepository;
+        private readonly IUserRepository<UserEntity> userRepository;
+
+        public AuthService(IRepository<AuthInfoEntity> authInfoRepository, IUserRepository<UserEntity> userRepository)
+        {
+            this.authInfoRepository = authInfoRepository;
+            this.userRepository = userRepository;
+        }
+
         async public Task<bool> CheckPassword(AuthInfoModel signinData)
         {
-            throw new NotImplementedException();
+            var user = await userRepository.Get(signinData.Email);
+            if (user is null) { return false; }
+
+            var authInfo = await authInfoRepository.Get(user.AuthInfoId);
+            var passwordHash = authInfo.PasswordHash;
+
+            return passwordHash == signinData.Password;
         }
 
         async public Task AddUser(AuthInfoModel newUser)
         {
-            throw new NotImplementedException();
+            var checkUser = await userRepository.Get(newUser.Email);
+            if (checkUser is not null) { return; }
+
+            var user = new UserEntity
+            {
+                Email = newUser.Email,
+                AuthInfo = new AuthInfoEntity
+                {
+                    PasswordHash = newUser.Password
+                },
+                Profile = new ProfileEntity
+                {
+                    Nickname = newUser.Email
+                }
+            };
+
+            await userRepository.Add(user);
         }
 
         async public Task<bool> Contains(AuthInfoModel newUser)
         {
-            throw new NotImplementedException();
+            var user = await userRepository.Get(newUser.Email);
+
+            if (user is null)
+                return true;
+            else
+                return false;
         }
     }
 }
