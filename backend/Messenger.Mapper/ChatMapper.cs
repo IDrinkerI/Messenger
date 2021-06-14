67b73c0c62312e1e74365ec14f528896ac3dbd46
Drinker <drink0.5l@gmail.com>
2021-06-14 19:07:21 +0300

    added initial ChatMapper

---
+++ backend/Messenger.Mapper/ChatMapper.cs
@@ -0,0 +1,24 @@
+﻿using Messenger.Data.Entities;
+using Messenger.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Messenger.Mapper
+{
+    public static class ChatMapper
+    {
+        public static ChatEntity ToEntity(this ChatModel chatModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static ChatModel ToModel(this ChatEntity chatEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
