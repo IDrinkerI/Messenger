c8a75c240f735b1ecc4325ffcbf407c5a684c309
Drinker <drink0.5l@gmail.com>
2021-06-14 18:05:32 +0300

    added initial ProfileMapper

---
+++ backend/Messenger.Mapper/ProfileMapper.cs
@@ -0,0 +1,24 @@
+﻿using Messenger.Data.Entities;
+using Messenger.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Messenger.Mapper
+{
+   public static class ProfileMapper
+    {
+        public static ProfileEntity ToEntity(this ProfileModel profileModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static ProfileModel ToModel(this ProfileEntity profileEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
