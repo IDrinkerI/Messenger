2f1246e445b270a8b8abc54afc0660d24c8c5201
Drinker <drink0.5l@gmail.com>
2021-06-14 18:31:29 +0300

    added initial MessageMapper

---
+++ backend/Messenger.Mapper/MessageMapper.cs
@@ -0,0 +1,24 @@
+﻿using Messenger.Data.Entities;
+using Messenger.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Messenger.Mapper
+{
+    public static class MessageMapper
+    {
+        public static MessageEntity ToEntity(this MessageModel messageModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static MessageModel ToModel(this MessageEntity messageEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
