101ee71d48709a8f0e62d7135fbeb3eda990ac42
Drinker <drink0.5l@gmail.com>
2021-07-03 07:33:28 +0300

    create initial ChatServiceTests

---
+++ backend/Messenger.Service.Tests/Services/ChatServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using Messenger.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Messenger.Service.Tests
+{
+    public class ChatServiceTests
+    {
+        #region AddChat
+
+
+
+        #endregion
+
+
+        #region GetChats
+
+
+
+        #endregion
+    }
+}
