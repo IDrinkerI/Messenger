53549204ca286179d35ac446d3d53b1623898ff1
Drinker <drink0.5l@gmail.com>
2021-07-04 05:27:41 +0300

    ChatEntity now contains User and UserId

--- backend/Messenger.Data/Entities/ChatEntity.cs
+++ backend/Messenger.Data/Entities/ChatEntity.cs
@@ -6,6 +6,8 @@ namespace Messenger.Data
     public class ChatEntity : BaseEntity
     {
         public string Name { get; set; }
+        public int UserId { get; set; }
+        public UserEntity User { get; set; }
 
         public List<MessageEntity> Messages { get; set; }
     }
